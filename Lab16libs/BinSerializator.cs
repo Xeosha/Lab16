c1ad0be7793f920befa2aac03c6dc14ae0ac871c
Daniel <109627577+Xeosha@users.noreply.github.com>
2024-12-08 01:06:25 +0500

    Add files via upload

---
+++ Lab16libs/BinSerializator.cs
@@ -0,0 +1,32 @@
+﻿
+using System.Runtime.Serialization.Formatters.Binary;
+
+
+namespace Lab16.Serialization
+{
+    #pragma warning disable SYSLIB0011
+    public class BinSerializator<T> : ISerializator<T>
+    {
+        public string FileType { get => "dat"; }
+        private readonly BinaryFormatter _binFormatter = new();
+
+        public T? Load(string filePath)
+        {
+            using FileStream fileStream = new (filePath, FileMode.Open);
+            return (T?)_binFormatter.Deserialize(fileStream);
+        }
+
+        public void Save(T objNeedToSave, string filePath)
+        {
+            using FileStream fileStream = new (filePath, FileMode.Create);
+            _binFormatter.Serialize(fileStream, objNeedToSave);
+        }
+
+        public async void SaveAsync(T objNeedToSave, string filePath)
+        {
+            await Task.Run(() => Save(objNeedToSave, filePath));
+        }
+#pragma warning restore SYSLIB0011
+    }
+
+}
---
+++ Lab16libs/ISerializator.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace Lab16.Serialization
+{ 
+    public interface ISerializator<T>
+    {
+        public string FileType { get; }
+
+        public T? Load(string filePath);
+
+        public void Save(T objNeedToSave, string filePath);
+    }
+
+}
